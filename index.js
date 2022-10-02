69a408594d22296b9b384661a7888e02564e62f8
Jeellow <111993006+Jeellow@users.noreply.github.com>
2022-10-02 18:00:49 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const KEY = `d022da54-f5f5-4327-a3b0-c444576a1742`;
 const NAMESPACE = "jeellow.github.io";
 const COUNT_URL = `https://api.countapi.xyz`;
 
-const counter = document.querySelectorAll('span');
+const counter = document.querySelectorAll('count');
 
 const getCount = async () => {
   const response = await fetch(`${COUNT_URL}/get/${NAMESPACE}/${KEY}`);
@@ -17,7 +17,7 @@ const incrementCount = async () => {
 };
 
 const setValue = (num) => {
-  var str = num.toString().padStart(6, "0");
+  var str = num.toString().padStart(5, "0");
   for (let index = 0; index < str.length; index++) {
     const element = str[index];
     counter[index].innerHTML = element;
